d7aaea8477eaa5fde82fd45bbc497f62cff1970b
Zemke <florian@zemke.io>
2021-03-06 20:57:42 +0100

    Less code

--- flori/19.py
+++ flori/19.py
@@ -1,54 +1,24 @@
-import re
-
-
-def parse_input(ll):
-  R = {}
-  messages = []
-  reading_grammar = True
-  for l in ll:
-    if l == '':
-      reading_grammar = False
-    elif reading_grammar:
-      split = l.split(': ')
-      if re.compile(r'"[ab]"').match(split[1]):
-        R[split[0]] = split[1][1]
-      else:
-        rr = split[1].split(' | ')
-        R[split[0]] = []
-        for r in rr:
-          R[split[0]].append(r.split(' '))
-    else:
-      messages.append(l)
-  return [R, messages]
-
-
 def matchstr(R, g, mm, idx, debug=False):
   """Return matched substring, -1 if there's no match."""
-  potentially_terminating = R[g]
-  terminating = isinstance(potentially_terminating, str)
-  if terminating:
-    if mm[idx] == potentially_terminating:
-      return potentially_terminating
-    else:
-      return -1
-  else:
+  R[g] = R[g]
+  if isinstance(R[g], str):
+    return R[g] if mm[idx] == R[g] else -1
+  for arr in R[g]:
     acc = ''
-    for arr in potentially_terminating:
-      for g1 in arr:
-        match = matchstr(R, g1, mm, idx+len(acc))
-        if match == -1:
-          acc = ''
-          break
-        else:
-          acc += match
-      if acc != '':
-        return acc
-    return -1
+    for g1 in arr:
+      match = matchstr(R, g1, mm, idx+len(acc))
+      if match == -1:
+        acc = ''
+        break
+      else:
+        acc += match
+    if acc != '':
+      return acc
+  return -1
 
 
 def check(R, message):
   mm = [m for m in message]
-  acc = ''
   limit_idx = None
   if len(R["0"]) > 1:
     raise Exception("that's unexpected")
@@ -62,7 +32,23 @@ def check(R, message):
 
 
 def do(ll):
-  [R, messages] = parse_input(ll)
+  R = {}
+  messages = []
+  reading_grammar = True
+  for l in ll:
+    if l == '':
+      reading_grammar = False
+    elif reading_grammar:
+      split = l.split(': ')
+      if split[1] == '"a"' or split[1] == '"b"':
+        R[split[0]] = split[1][1]
+      else:
+        rr = split[1].split(' | ')
+        R[split[0]] = []
+        for r in rr:
+          R[split[0]].append(r.split(' '))
+    else:
+      messages.append(l)
   for message in messages:
     res = check(R, message)
     print(res, message)
