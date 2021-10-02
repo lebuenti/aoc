f26e9e7e96716cefd0e2cadf8d37aee67d55de82
Zemke <florian@zemke.io>
2021-10-02 13:29:34 +0200

    Master of the universe

--- flori/19.py
+++ flori/19.py
@@ -1,37 +1,36 @@
-def matchstr(R, g, mm, idx, debug=False):
-  """Return matched substring, -1 if there's no match."""
-  R[g] = R[g]
-  if isinstance(R[g], str):
-    return R[g] if mm[idx] == R[g] else -1
-  for arr in R[g]:
+#!/usr/bin/env python
+
+
+recc = ["8", "11"]
+
+
+def matchstr(R, rk, message, m_idx):
+  if m_idx >= len(message):
+    # exceeded target string
+    return -1
+  if isinstance(R[rk], str):
+    # grammar terminal char
+    if message[m_idx] == R[rk]:
+      return R[rk]
+    else:
+      return -1
+  for rr in R[rk]:
     acc = ''
-    for g1 in arr:
-      match = matchstr(R, g1, mm, idx+len(acc))
+    for rk1 in rr:
+      match = matchstr(R, rk1, message, m_idx + len(acc))
       if match == -1:
         acc = ''
         break
-      else:
-        acc += match
+      acc += match
+      if message.endswith(acc) and m_idx + len(acc) == len(message):
+        if rr.index(rk1) == len(rr) - 1 or rk1 in recc:
+          return acc
     if acc != '':
       return acc
   return -1
 
 
-def check(R, message):
-  mm = [m for m in message]
-  limit_idx = None
-  if len(R["0"]) > 1:
-    raise Exception("that's unexpected")
-  acc = ''
-  for g in R["0"][0]:
-    matches = matchstr(R, g, mm, len(acc), True)
-    if matches == -1:
-      return False
-    acc += matches
-  return acc == message
-
-
-def do(ll):
+def do(ll, part):
   R = {}
   messages = []
   reading_grammar = True
@@ -49,12 +48,17 @@ def do(ll):
           R[split[0]].append(r.split(' '))
     else:
       messages.append(l)
+  if part == 2:
+    R["8"] = [["42"], ["42", "8"]]
+    R["11"] = [["42", "31"], ["42", "11", "31"]]
+  res = 0
   for message in messages:
-    res = check(R, message)
-    print(res, message)
+    res += (matchstr(R, "0", message, 0) == message)
+  print("part", part, "matches", res)
 
 
-with open('19.in', 'r') as f:
-  ll = f.read().splitlines()
-  do(ll)
+for part in range(2):
+  with open('19.in', 'r') as f:
+    ll = f.read().splitlines()
+    do(ll, part + 1)
 
