9b02d45221f2719f9c51b0a0d2768cba009917be
Zemke <florian@zemke.io>
2021-12-09 12:34:43 +0100

    Day 9

---
+++ aoc21/flori/9.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+def neigh(G, r, c, idx=False):
+  res = []
+  if r-1 >= 0:
+    res.append((r-1, c) if idx else G[r-1][c])
+  if r+1 < len(G):
+    res.append((r+1, c) if idx else G[r+1][c])
+  if c-1 >= 0:
+    res.append((r, c-1) if idx else G[r][c-1])
+  if c+1 < len(G[r]):
+    res.append((r, c+1) if idx else G[r][c+1])
+  return res
+
+
+def j1():
+  lvl = 0
+
+  for r in range(len(G)):
+    for c in range(len(G[r])):
+      nn = neigh(G, r, c)
+      if len([n for n in nn if n > G[r][c]]) == len(nn):
+        lvl += 1 + G[r][c]
+
+  return lvl
+
+
+def flow(G, r, c):
+  vis = []
+  s = [(r,c)]
+  c = 0
+  while len(s) > 0:
+    nr, nc = s.pop()
+    if G[nr][nc] != 9 and (nr, nc) not in vis:
+      c += 1
+      s.extend(neigh(G, nr, nc, idx=True))
+    vis.append((nr,nc))
+  return c
+
+
+def j2():
+  baisins = []
+  for r in range(len(G)):
+    for c in range(len(G[r])):
+      nn = neigh(G, r, c)
+      nn_idx = neigh(G, r, c, idx=True)
+      if len([n for n in nn if n > G[r][c]]) == len(nn):
+        baisins.append(flow(G, r, c))
+  return sorted(baisins, reverse=True)[:3]
+
+
+with open('9.in', 'r') as f:
+  ll = f.read().splitlines()
+
+G = []
+for l in ll:
+  G.append([int(i) for i in [_ for _ in l]])
+
+
+print('1', j1())
+
+resj2 = j2()
+print('2', resj2[0] * resj2[1] * resj2[2])
+
