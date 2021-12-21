bb22f9c1401dc16c3f87eca4008aabc253d1f54e
Zemke <florian@zemke.io>
2021-12-21 22:14:39 +0100

    Day 20 cleanup

--- aoc21/flori/20.py
+++ aoc21/flori/20.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
-
 with open('20.in', 'r') as f:
   alg, img  = f.read().split("\n\n")
   img = img.splitlines()
 
-print(alg)
-print(img)
-
-def nice(img):
-  for i in img:
-    print(i)
-  print()
 
 def pad(img, p=3, s='.'):
   pad = [*img]
@@ -22,18 +14,13 @@ def pad(img, p=3, s='.'):
     pad[y] = (s*p) + pad[y] + (s*p)
   return pad
 
-print('xx', alg[33], alg[34], alg[35])
-print()
-
 I = pad(img)
-nice(I)
 
 for c in range(50):
   O = []
   for y in range(len(I)-2):
     O.append('')
     for x in range(len(I[y])-2):
-      #print(I[y][x], end='')
       w = [
         I[y][x:x+3],
         I[y+1][x:x+3],
@@ -43,11 +30,6 @@ for c in range(50):
       dec = int(binstr,2)
       v = alg[dec]
       O[-1] += v
-      #print(binstr, dec, v)
-      #print()
-      #for x in w:
-      #  print(x)
-      #prfnt()
 
   if c % 2 != 0:
     #print('yep')
@@ -55,5 +37,4 @@ for c in range(50):
   else:
     I = pad(O,3,'#')
   print(c+1, ''.join(I).count('#'))
-  nice(I)
 
