1f1048d664d534b53451f41a567c7259a01ecf57
Zemke <florian@zemke.io>
2021-12-22 23:15:49 +0100

    Day 19 both parts

--- aoc21/flori/19.py
+++ aoc21/flori/19.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
-import numpy as np
-from tqdm import tqdm
-
+from tqdm import trange
+from collections import defaultdict
 
 def permute():
   yield lambda a, b, c: (+a, +b, +c)
@@ -33,93 +32,92 @@ def permute():
   yield lambda a, b, c: (-b, -a, -c)
   yield lambda a, b, c: (-a, -c, -b)
 
-inp = '19.in'
+def create_diff(a, b):
+  return a[0]-b[0], a[1]-b[1], a[2]-b[2]
 
-with open(inp, 'r') as f:
+def apply_diff(a, b):
+  return a[0]+b[0], a[1]+b[1], a[2]+b[2]
+
+with open('19.in', 'r') as f:
   ll = f.read().splitlines()
 
-scanners = []
+sc = []
 for l in ll:
   if l.startswith('---'):
-    scanners.append([])
+    sc.append([])
   elif l == '':
     continue
   elif l[0].isdigit() or l[0] == '-':
     x,y,z = [int(n) for n in l.split(',')]
-    scanners[-1].append(np.array([x,y,z]))
+    sc[-1].append((x,y,z))
 
+# ground-truth beacons
+gt_sc = [None] * len(sc)
+gt_sc[0] = [*sc[0]]
 
-beacons = [*scanners[0]]
-sc_positions = [(0,0,0)]
+# ground-truth scanner positions
+gt_sc_pos = [None] * len(sc)
+gt_sc_pos[0] = (0,0,0)
 
-T = 1000  # max distance for a scanner to discover beacons
+DP = defaultdict(lambda: True)
+while gt_sc.count(None):
+  bef = gt_sc.count(None)
+  for i in (progr := trange(len(sc))):
+    if gt_sc[i] is not None:
+      continue  # scanner already ground-truthed
 
-# sc * perms * beacs * sc * sc * beacs
-for c, sc in enumerate(scanners[1:]):
-  perm_mx_matches = 0
-  perm_mn_unseen = []
+    # match unknown sc against ground-truthed sc
+    for gt_i in range(len(gt_sc)):
+      if gt_sc[gt_i] is None:
+        continue
+      if gt_i == i:
+        continue
+      if not DP[gt_i, i]:
+        continue
 
-  for perm in (progr := tqdm(permute(), total=24)):
-    mx_matches, mn_unseen = 0, []
-    progr.set_description(
-      f"{c+1} of {len(scanners)-1} scanners, " \
-      f"{len(beacons)} beacons")
-    for ri in range(len(beacons)):
-      for i in range(len(sc)):
-        stck = np.stack([perm(*sc[i]), beacons[ri]])
-        diff = np.diff(stck, axis=0).squeeze()
-        sc_pos = np.zeros(3, int) + diff
+      mx_perm, mx_perm_match_count, mx_perm_diff = None, -1, None
+      for perm in permute():
+        mx_diff, mx_match_count = None, -1
+        for bi in range(len(sc[i])):
+          for gt_bi in range(len(gt_sc[gt_i])):
+            diff = create_diff(gt_sc[gt_i][gt_bi], perm(*sc[i][bi]))
+            match_count = 1
+            for bj in range(len(sc[i])):
+              upd = apply_diff(perm(*sc[i][bj]), diff)
+              for gt_bj in range(len(gt_sc[gt_i])):
+                if gt_bj == gt_bi:
+                  continue
+                if upd == gt_sc[gt_i][gt_bj]:
+                  match_count += 1
+            if match_count > mx_match_count:
+              mx_match_count = match_count
+              mx_diff = diff
+        if mx_match_count > mx_perm_match_count:
+          mx_perm = perm
+          mx_perm_match_count = mx_match_count
+          mx_perm_diff = mx_diff
 
-        already, unseen, matches = [ri], [], 1
-        range_beacs = []
-        for beac in beacons:
-          dists = np.absolute(np.diff(np.stack([beac, sc_pos]), axis=0))
-          if np.all(dists <= T):
-            range_beacs.append(beac)
-          if len(range_beacs) >= 12:
-            break
-        for k in range(len(sc)):
-          if k == i:
-            continue
-          upd = perm(*sc[k]) + diff
-          matches_bef = matches
-          for rk in range(len(range_beacs)):
-            if rk in already:
-              continue
-            if np.all(upd == range_beacs[rk]):
-              already.append(rk)
-              matches += 1
-          if matches_bef == matches:
-            unseen.append(upd)
-
-          if matches + (len(sc)-(k+1)) * (len(range_beacs)-(rk+1)) < mx_matches:
-            break
-
-        if matches > mx_matches:
-          mx_matches = matches
-          mn_unseen = unseen
-          f_sc_pos = sc_pos
-
-    if mx_matches > perm_mx_matches:
-      perm_mx_matches = mx_matches
-      perm_mn_unseen = mn_unseen
-      perm_f_sc_pos = f_sc_pos
-
-      if mx_matches >= 12:
+      if mx_perm_match_count >= 12:
+        gt_sc[i] = [apply_diff(mx_perm(*beac),mx_perm_diff) for beac in sc[i]]
+        gt_sc_pos[i] = mx_perm_diff
+        progr.set_description(str(gt_sc.count(None)) + " left")
         break
+      DP[gt_i, i] = False
 
-  sc_positions.append(tuple(perm_f_sc_pos.tolist()))
-  for un in perm_mn_unseen:
-    exist = False
-    for b in beacons:
-      if np.all(un == b):
-        exist = True
-        break
-    if not exist:
-      beacons.append(un)
+    if bef != gt_sc.count(None):
+      break  # seems faster than continue
 
+print(len(set([y for x in gt_sc for y in x])))
 
-print('sc_positions', sc_positions)
-print(beacons)
-print(len(beacons), 'beacons')
+mx = -1
+for i in range(len(gt_sc_pos)):
+  for k in range(len(gt_sc_pos)):
+    if i == k:
+      continue
+    n = abs(gt_sc_pos[i][0]-gt_sc_pos[k][0]) \
+      + abs(gt_sc_pos[i][1]-gt_sc_pos[k][1]) \
+      + abs(gt_sc_pos[i][2]-gt_sc_pos[k][2])
+    mx = max(mx, n)
+
+print(mx)
 
