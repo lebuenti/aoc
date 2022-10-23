ce293bd2d00fa96f7d11b5a9fb9043f300ad2813
Zemke <florian@zemke.io>
2022-10-23 12:40:50 +0200

    2019 13

---
+++ aoc19/flori/13.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from intcode import IntCode
+from collections import defaultdict
+
+r = open('13.in', 'r').read()
+
+ic = IntCode(r)
+out = ic()
+assert ic.done()
+B = {}
+for i in range(0, len(out), 3):
+  x,y,ident = out[i:i+3]
+  if ident == 2:
+    B[x,y] = ident
+print('part1:', len(B))
+
+mxx, mxy = 0, 0
+score = None
+G = []
+init = False
+ic = IntCode(','.join(['2', *r.split(',')[1:]]))
+while 2 in [g[2] for g in G] or not init:
+  out = ic()
+  for i in range(0, len(out), 3):
+    x,y,ident = out[i:i+3]
+    if x == -1 and y == 0:
+      score = ident 
+    else:
+      if not init:
+        G.append([x,y,ident])
+        mxx = max(mxx, x)
+        mxy = max(mxy, y)
+      else:
+        for i in range(len(G)):
+          gx,gy,_ = G[i]
+          if gx == x and gy == y:
+            G[i][2] = ident
+            break
+  init = True
+  # print
+  #for y in range(mxy+1):
+  #  for x in range(mxx+1):
+  #    for gx,gy,ident in G:
+  #      if gx == x and gy == y:
+  #        print(ident or ' ', end='')
+  #  print()
+  #print()
+  ball = [g for g in G if g[2] == 4][0]
+  paddle = [g for g in G if g[2] == 3][0]
+  ic.inputs = \
+    [-1 if paddle[0] > ball[0] else 0 if paddle[0] == ball[0] else +1]
+print('part2:', score)
+
+
