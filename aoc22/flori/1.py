fcacd7160fa11c0905f49c190fcfb73c9472aed0
Zemke <florian@zemke.io>
2022-12-01 10:53:44 +0100

    Easiest

--- aoc22/flori/1.py
+++ aoc22/flori/1.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
 
 ll = open('1.in', 'r').read().splitlines()
-mx = -1
-rng = [None] * 3
-tops = [-1] * 3
-for i in range(len(tops)):
-  c = 0
-  cnt = 0
-  for l in ll:
-    if l == "":
-      cnt += 1
-      if c > mx and not(cnt in rng[:i]):
-        mx = c
-        rng[i] = cnt
-      c = 0
-    else:
-      c += int(l)
-  tops[i] = mx
-  mx = -1
+sums = []
+c = 0
+for l in ll:
+  if l == "":
+    sums.append(c)
+    c = 0
+  else:
+    c += int(l)
 
-print('part1:', tops[0])
-print('part2:', sum(tops))
+res = sorted(sums)[::-1][:3]
+print('part1:', res[0])
+print('part2:', sum(res))
 
