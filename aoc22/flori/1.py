b47aa8ee3352456af9997c1d4d08a2ebdf027175
Zemke <florian@zemke.io>
2022-12-01 11:02:35 +0100

    Easiestest

--- aoc22/flori/1.py
+++ aoc22/flori/1.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
 
-ll = open('1.in', 'r').read().splitlines()
-sums = []
-c = 0
-for l in ll:
-  if l == "":
-    sums.append(c)
-    c = 0
-  else:
-    c += int(l)
-
-res = sorted(sums)[::-1][:3]
-print('part1:', res[0])
-print('part2:', sum(res))
+ll = open('1.in','r').read().split("\n\n")
+x = sorted(sum([int(n) for n in l.split()]) for l in ll)[::-1][:3]
+print(x[0])
+print(sum(x))
 
