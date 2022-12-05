0e1a4fc0ff899ef32b84f7af5aef72dc59842a29
Zemke <florian@zemke.io>
2022-12-05 11:33:08 +0100

    golf it

--- aoc22/flori/5.py
+++ aoc22/flori/5.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
-ll = open('5.in').read().split("\n\n")
-mm = [l.split(' ') for l in ll[1].splitlines()]
-
-for i in range(len(mm)):
-  mm[i] = tuple([int(m) for m in mm[i] if m.isdigit()])
+stacks,moves = open('5.in').read().split("\n\n")
 
 for part in (1,2):
-  S = [0, *[[] for _ in range(int(ll[0].strip()[-1]))]]
-  for l in ll[0].splitlines()[:-1]:
+  S = [0, *[[] for _ in range(int(stacks.strip()[-1]))]]
+  for l in stacks.splitlines()[:-1]:
     idx = 1
     for i in range(1,len(l),4):
       if l[i] != ' ':
         S[idx].append(l[i])
       idx += 1
 
-  for num,fr,to in mm:
+  for m in [l.split(' ') for l in moves.splitlines()]:
+    num,fr,to = (int(m1) for m1 in m if m1.isdigit())
     mv = S[fr][:num]
     if part == 1:
       mv = mv[::-1]
