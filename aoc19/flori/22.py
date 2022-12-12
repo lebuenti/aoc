c5204d9701775ade1f550c73a0d612d4794f1f4d
Zemke <florian@zemke.io>
2022-12-12 19:16:57 +0100

    2019/22/1

---
+++ aoc19/flori/22.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from collections import deque
+
+ll = open('22.in').read().splitlines()
+
+T = 10006
+S = deque(list(range(0,T+1)))
+
+for l in ll:
+  if l == "deal into new stack":
+    # in-place
+    S.reverse()
+  elif l.startswith("cut "):
+    # in-place
+    cut = int(l.split(' ')[1])
+    for _ in range(abs(cut)):
+      if cut > 0:
+        S.append(S.popleft())
+      else:
+        S.appendleft(S.pop())
+  elif l.startswith("deal with increment "):
+    # inst
+    N = int(l.split(' ')[-1])
+    lenS = len(S)
+    nS = deque([None for _ in range(lenS)])
+    pos = 0
+    while S:
+      assert nS[pos % lenS] is None
+      nS[pos % lenS] = S.popleft()
+      pos += N
+    S = nS
+    assert all([s is not None for s in S])
+
+print(S.index(2019))
+
