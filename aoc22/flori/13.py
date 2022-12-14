52f8391faf2b62b9f0d316ae9bf05a7e38d9e79a
Zemke <florian@zemke.io>
2022-12-14 13:29:35 +0100

    2022/13 It's a stack

--- aoc22/flori/13.py
+++ aoc22/flori/13.py
@@ -10,9 +10,9 @@ def zipfill(v1,v2):
   return list(zip(*[[None if z >= len(v) else v[z] for z in range(0, max(len(v1),len(v2)))] for v in (v1,v2)]))
 
 def srt(a,b):
-  Q = deque(zipfill(a,b))
-  while Q:
-    v1,v2 = Q.popleft()
+  S = deque(zipfill(a,b))
+  while S:
+    v1,v2 = S.popleft()
     assert not (v1 is None and v2 is None)
     if isinstance(v1, int) and isinstance(v2, int):
       if v1 < v2:
@@ -20,14 +20,14 @@ def srt(a,b):
       elif v1 > v2:
         return -1
     elif isinstance(v1, list) and isinstance(v2, list):
-      Q.extendleft(zipfill(v1,v2)[::-1])
+      S.extendleft(zipfill(v1,v2)[::-1])
     elif v1 is None:
       return 1
     elif v2 is None:
       return -1
     else:
       fll = ([v1],v2) if isinstance(v1, int) else (v1,[v2])
-      Q.extendleft(zipfill(*fll)[::-1])
+      S.extendleft(zipfill(*fll)[::-1])
 
 print(sum(min(1, srt(*ll[i:i+2]) + 1) * ((i + 2) // 2) for i in range(0, len(ll), 2)))
 
