8c42241f3895623fac15742749154526e8966c88
Zemke <florian@zemke.io>
2022-12-14 15:45:48 +0100

    2019/22/1

--- aoc19/flori/22.py
+++ aoc19/flori/22.py
@@ -4,14 +4,13 @@ from collections import deque
 
 ll = open('22.in').read().splitlines()
 
-T = 10006
+L = 10007
+S = deque(list(range(0,L)))
 
 for l in ll:
   if l == "deal into new stack":
     # in-place
-    print(S[100])
     S.reverse()
-    print(S[100])
   elif l.startswith("cut "):
     # in-place
     cut = int(l.split(' ')[1])
@@ -32,6 +31,29 @@ for l in ll:
       pos += N
     S = nS
     assert all([s is not None for s in S])
-
 print(S.index(2019))
+
+# again without actually maintaining the stack
+now = 2019
+for l in ll:
+  if l == "deal into new stack":
+    now = L-1-now
+  elif l.startswith("cut "):
+    cut = int(l.split(' ')[1])
+    if cut > 0:
+      if cut >= now+1:
+        now = L-1-(cut-(now+1))
+      else:
+        now = now-cut
+    else:
+      if now >= L-abs(cut):
+        now = abs(cut)-(L-now)
+      else:
+        now = now-cut
+  elif l.startswith("deal with increment "):
+    N = int(l.split(' ')[-1])
+    now = (now*N) % L
+print(now)
+
+# TODO part 2 is too hard
 
