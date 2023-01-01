9964d88a8d986bc158da602c9e42c7bf9b1c6f19
Zemke <florian@zemke.io>
2023-01-01 23:30:02 +0100

    2018/11 improv

--- aoc18/flori/11.py
+++ aoc18/flori/11.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-SERIAL = 9810
+SERIAL = int(open('11.in').read().strip())
 N = 300
 
 P = {}
+MXP = -1
 for y in range(N):
   for x in range(N):
     rack_id = x + 10
     P[x,y] = int(str(((rack_id * y) + SERIAL) * rack_id).rjust(3,'0')[-3]) - 5
+    MXP = max(P[x,y], MXP)
 
 # part 1
 mx = -1
@@ -35,21 +37,21 @@ mx = -1
 res = None
 for y in range(N):
   for x in range(N):
-    minx = maxx = x
-    miny = maxy = y
     sm = P[x,y]
     for s in range(N):
       if sm > mx:
         mx = sm
         res = x, y, s+1
-      if maxx+1 == N or maxy+1 == N:
+      if x+s+1 == N or y+s+1 == N:
         break
-      for hy in range(miny, maxy+1):
-        sm += P[maxx+1, hy]
-      for hx in range(minx, maxx+1):
-        sm += P[hx, maxy+1]
-      maxx += 1
-      maxy += 1
-      sm += P[maxx, maxy]
+      prev = sm
+      for hy in range(y, y+s+1):
+        sm += P[x+s+1, hy]
+      for hx in range(x, x+s+1):
+        sm += P[hx, y+s+1]
+      sm += P[x+s+1, y+s+1]
+      if prev - sm > MXP * 10:
+        # wild guess to improve performance
+        break
 print(f"{res[0]},{res[1]},{res[2]}")
 
