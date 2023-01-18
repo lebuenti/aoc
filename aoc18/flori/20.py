c956abce4bacdeff6dce6788b405fbc7aa5af490
Zemke <florian@zemke.io>
2023-01-18 16:00:44 +0100

    2018/20 Just keep rooms and doors

--- aoc18/flori/20.py
+++ aoc18/flori/20.py
@@ -11,17 +11,19 @@ L = ('[["' + L[1:-1] + '"]]') \
   .replace("|", '"],["') \
   .replace('],]', ']]') \
   .replace(',,', ',')
+# eval(L) causes SyntaxError: too many nested parentheses
+#  somehow doesn't happen with json.loads(L)
 L = deque(json.loads(L))
 
-G = {(0,0): "X"}
+DOOR = set()
+ROOM = set()
 NESW = {"N": (0,-1), "E": (+1,0), "S": (0,+1), "W": (-1,0)}
 Q = deque([((0,0), L.popleft(), L)])
 seen = set()
 while Q:
   (x,y), q, l = Q.popleft()
   if not isinstance(q, str):
-    X = (x,y), q[0], deque([*q[1:], *l])
-    Q.appendleft(X)
+    Q.appendleft(((x,y), q[0], deque([*q[1:], *l])))
     continue
 
   if ((x,y), q, str(l)) in seen:
@@ -30,38 +32,26 @@ while Q:
 
   for d in q:
     nx,ny = x + NESW[d][0], y + NESW[d][1]
-    if d == "E" or d == "W":
-      G[nx,ny] = "|"
-    elif d == "N" or d == "S":
-      G[nx,ny] = "-"
-    else:
-      raise Exception(d)
+    DOOR.add((nx,ny))
     x,y = nx + NESW[d][0], ny + NESW[d][1]
-    G[x,y] = '.'
-    G[x-1,y-1] = '#'
-    G[x+1,y+1] = '#'
-    G[x-1,y+1] = '#'
-    G[x+1,y-1] = '#'
+    ROOM.add((x,y))
 
   if len(l) == 0:
     continue
 
   if isinstance(l[0], str):
-    N = (x,y), l[0], deque(list(l)[1:])
-    Q.append(N)
+    Q.append(((x,y), l[0], deque(list(l)[1:])))
   else:
     for n in l.popleft():
-      N = (x,y), n, l
-      Q.append(N)
+      Q.append(((x,y), n, l))
 
 K = {(0,0): 0}
 Q = deque([(0, (0,0))])
 while Q:
   dist, (x,y) = Q.popleft()
-  for nx,ny in [(x+dx*2, y+dy*2) for dx,dy in NESW.values() if (x+dx,y+dy) in G and G[x+dx,y+dy] != '#']:
+  for nx,ny in [(x+dx*2, y+dy*2) for dx,dy in NESW.values() if (x+dx,y+dy) in DOOR]:
     cost = dist + 1
     if (nx,ny) not in K or cost < K[nx,ny]:
-      assert G[nx,ny] == '.'
       K[nx,ny] = cost
       Q.append((cost, (nx,ny)))
 
