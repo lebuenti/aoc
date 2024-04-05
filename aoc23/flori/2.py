23c1d8d8e50a68c81c70be626fbb6d724bb1a6b1
Zemke <florian@zemke.io>
2024-04-05 20:39:15 +0200

    2023/02/2

--- aoc23/flori/2.py
+++ aoc23/flori/2.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python3
 
-from collections import Counter
+from collections import Counter, defaultdict
+from math import prod
 
 L = open('2.in').read()
 ll = L.splitlines()
 
 T = Counter({'red': 12, "green": 13, "blue": 14})
-works = 0
+works = sm = 0
 for l in ll:
   g, nn = l.split(": ")
   g = int(g.split(" ")[1])
   yes = True
+  C = defaultdict(int)
   for n1 in nn.split("; "):
     c = Counter({v: int(k) for k,v in [tuple(n.split(" ")) for n in n1.split(", ")]})
+    for ckey in c.keys():
+      C[ckey] = max(c[ckey], C[ckey])
     for k,v in c.items():
       if T[k] < v:
         yes = False
         break
-    if not yes:
-      break
   works += g * yes
+  sm += prod(C.values())
+
 print(works)
+print(sm)
 
